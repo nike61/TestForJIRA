c75c1cc29acf8cc3ac2f5f426fc6ebbd0de27bb9
nike61 <ilyin@polymedia.ru>
2012-12-08 16:25:27 +0400

    Done TFJ-1

--- Sources/TestForJIRA/TestForJIRA/Program.cs
+++ Sources/TestForJIRA/TestForJIRA/Program.cs
@@ -9,7 +9,7 @@ namespace TestForJIRA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Version 1.0.0.2");
+            Console.WriteLine("Version 1.0.0.3 TFJ-1");
         }
     }
 }
